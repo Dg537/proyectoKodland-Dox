4450145acf969c1a37c34627c24cec71ff7a9f50
Dg537 <gabriel.x-box@outlook.com>
2025-05-13 12:05:14 -0600

    DoxBot.py

--- DoxBot.py
+++ DoxBot.py
@@ -1,12 +1,10 @@
-import discord
+import discord, os, random
 from bot_logic import *
 from discord.ext import commands
 
 intents = discord.Intents.default() # almacenamiento de los privilegios del bot
 intents.message_content = True # Activar lectura de mensajes
 bot = commands.Bot(command_prefix="/",intents=intents) # Creacion de bot y transferencia de los privilegios
-
-comands = "/hola, /bye, /password, /emoji, /coin, /?, /help"
 
 @bot.event
 async def on_ready():
@@ -32,5 +30,22 @@ async def coin(ctx):
 async def whenJoined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def mem(ctx):
+    random_mem = random.choice(os.listdir("images"))
+    
+    with open(f'images/{random_mem}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 bot.run("aqui tu mmm... como se llame")
